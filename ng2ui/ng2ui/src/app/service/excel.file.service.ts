9c76953867d963bc960cc5f3ab4449223e3ec1c6
Pritesh Patel <3ppatel@gmail.com>
2017-01-22 14:55:01 -0500

    --adding code to make file upload call to backend

--- ng2ui/ng2ui/src/app/service/excel.file.service.ts
+++ ng2ui/ng2ui/src/app/service/excel.file.service.ts
@@ -7,6 +7,7 @@ import {Observable} from "rxjs";
 @Injectable()
 export class ExcelFileService{
     private url = "http://localhost:9999/api/download";
+    private importUrl = "http://localhost:9999/api/upload";
     constructor(private _http : Http){}
 
     exportFile() {
@@ -16,9 +17,22 @@ export class ExcelFileService{
             headers: new Headers()
         }).subscribe(
             (response) => {
-                var blob = new Blob([response.blob()], {type: 'application/e'});
+                var blob = new Blob([response.blob()], {type: 'application/zip'});
                 var filename = 'file.xls';
                 saveAs(blob, filename);
+            }
+        );
+    }
+
+    importFile(file : File){
+        this._http.post(this.importUrl, file,{
+            method: RequestMethod.Post,
+                headers: new Headers()
+        }).subscribe(
+            (response) => {
+                var res = response.json();
+                console.log('Response from import : ' + res);
+
             }
         );
     }
